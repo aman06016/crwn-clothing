502deaeb5ab0a0316ab2b499de26c0addab0cb1e
Aman gupta <aman06016@gmail.com>
2020-06-02 23:31:29 +0530

    added checkout page and populate with checkout items and added functionality of   increase and decrease quantity of respective items

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
+
 import {toggleCartHidden} from '../../redux/cart/cart.action';
 import {selectCartItemsCount} from '../../redux/cart/cart.selectors';
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
@@ -16,8 +18,8 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-const mapStatetoProps = state=> ({
-    itemCount : selectCartItemsCount(state)
+const mapStatetoProps = createStructuredSelector({
+    itemCount : selectCartItemsCount
 });
 
 export default connect(
