74a83e8350f93cd8686a8cbce81623aa09238cda
Aman gupta <aman06016@gmail.com>
2020-06-03 20:49:56 +0530

    moved out directory and shopdata to their own reducer , created respective selecters and updated coressponding components with new redux flow for directory/shop

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,18 @@
 import React from 'react';
-import SHOP_DATA from './shop.data.js';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import CollectionsOverview from '../../components/collection-overview/collection-overview.component';
+import CollectionPage from '../collection/collection.component';
+import {Route} from 'react-router-dom';
 
-class ShopPage extends React.Component{
+const  ShopPage =({match}) => {
 
-    constructor(props){
-        super(props);
-        this.state={
-            collections: SHOP_DATA
-        }
-    }
-
-    render(){
-        const {collections} =this.state;
-        return(
+    //console.log("aman");
+    //console.log(match);
+    return(
             <div className="shop-page">
-                {
-                    collections.map( ({id,items,title}) => (
-                        <CollectionPreview key = {id} items={items} title={title}/>
-                    )) 
-                }
+                <Route exact path = {`${match.path}`} component={CollectionsOverview} />
+                <Route path ={`${match.path}/:collectionId`} component={CollectionPage}/>
             </div>
         );
-    }
+    
 }
-
 export default ShopPage;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
-//import { persistStore } from 'redux-persist';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
@@ -10,9 +10,8 @@ const middlewares = [logger];
 //   middlewares.push(logger);
 // }
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-//export const persistor = persistStore(store);
+export const persistor = persistStore(store);
 
-//export default { store, persistStore };
-export default store;
+export default { store, persistor };
